12eb77a82e791deb1f12d65f123c422946a04540
JonathanVanWin <grandearbol@gmail.com>
2018-09-27 19:18:13 +0300

    Changing code

--- main.cpp
+++ main.cpp
@@ -10,16 +10,16 @@ Mat g_src, g_img_canny;
 void imageProcessing()
 {
 	Mat img_gray, postering;
-	fastNlMeansDenoising(g_src, g_src);
-	pyrMeanShiftFiltering(g_src, postering, 20, 40, 2);
+	//fastNlMeansDenoising(g_src, g_src);
+	//pyrMeanShiftFiltering(g_src, postering, 20, 40, 2);
 	cvtColor(g_src, img_gray, COLOR_BGR2GRAY);
 	//GaussianBlur(img_gray, img_gray, Size(5, 5), 0);
 	threshold(img_gray, img_gray, 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
 
-	erode(img_gray, img_gray, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)), Point(-1,-1),2);
+	//erode(img_gray, img_gray, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)), Point(-1,-1),2);
 	//dilate(img_gray, img_gray, getStructuringElement(MORPH_ELLIPSE, Size(3, 3)), Point(-1, -1), 1);
 	Canny(img_gray, g_img_canny, 120, 150, 3,true);
-	morphologyEx(g_img_canny, g_img_canny, MORPH_CLOSE, getStructuringElement(CV_SHAPE_RECT, Size(9, 9)));
+	//morphologyEx(g_img_canny, g_img_canny, MORPH_CLOSE, getStructuringElement(CV_SHAPE_RECT, Size(9, 9)));
 
 	imshow("Canny", g_img_canny);
 	imshow("Window", img_gray);
